f6738a78696e3fe62fe1e82aa698b2cda1a7376c
Cory House <housecor@gmail.com>
2016-01-14 09:13:30 -0600

    Opening built project on prod build so it can be easily reviewed.

---
+++ tools/distServer.js
@@ -0,0 +1,19 @@
+// This file configures a web server for testing the production build
+// on your local machine.
+
+var browserSync = require('browser-sync');
+
+// Run Browsersync
+browserSync({
+  port: 3000,
+  ui: {
+    port: 3001
+  },
+  server: {
+    baseDir: 'dist'
+  },
+
+  files: [
+    'src/*.html'
+  ]
+});
