7f6cae39bb89634d78c8457be3c6e885f28aa66f
Nick Taylor <nick@iamdeveloper.com>
2016-01-17 22:28:53 -0500

    Refactor.

--- src/components/FuelSavingsResults.js
+++ src/components/FuelSavingsResults.js
@@ -5,13 +5,13 @@ import NumberFormatter from '../businessLogic/numberFormatter';
 //More info: https://facebook.github.io/react/blog/2015/10/07/react-v0.14.html#stateless-functional-components
 //And https://medium.com/@joshblack/stateless-components-in-react-0-14-f9798f8b992d
 //And starter kit with more examples here: https://github.com/ericelliott/react-pure-component-starter
-let FuelSavingsResults = (props) => {
-    let savingsExist = NumberFormatter.scrubFormatting(props.savings.monthly) > 0;
-    let savingsClass = savingsExist ? 'savings' : 'loss';
-    let resultLabel = savingsExist ? 'Savings' : 'Loss';
+const FuelSavingsResults = (props) => {
+    const savingsExist = NumberFormatter.scrubFormatting(props.savings.monthly) > 0;
+    const savingsClass = savingsExist ? 'savings' : 'loss';
+    const resultLabel = savingsExist ? 'Savings' : 'Loss';
 
     //You can even exclude the return statement below if the entire component is
-    //composed within the parenthesies. Return is necessary here because some 
+    //composed within the parenthesies. Return is necessary here because some
     //variables are set above.
     return (
         <table>
@@ -40,7 +40,7 @@ let FuelSavingsResults = (props) => {
     );
 };
 
-//Note that this odd style is utilized for propType validation for now. Must be defined *after* 
+//Note that this odd style is utilized for propType validation for now. Must be defined *after*
 //the component is defined, which is why it's separate and down here.
 FuelSavingsResults.propTypes = {
     savings: PropTypes.object.isRequired
