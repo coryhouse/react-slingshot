e409c6180506b2480050467ec54903c0d217c590
Nick Taylor <nick@iamdeveloper.com>
2016-01-17 22:25:07 -0500

    Converted components to stateless components.

--- src/components/FuelSavingsApp.js
+++ src/components/FuelSavingsApp.js
@@ -2,79 +2,76 @@ import React, {PropTypes} from 'react';
 import FuelSavingsResults from './FuelSavingsResults';
 import FuelSavingsTextInput from './FuelSavingsTextInput';
 
-class FuelSavingsCalculatorForm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    //why all the binds below? See here: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
-    this.fuelSavingsKeypress = this.fuelSavingsKeypress.bind(this);
-    this.props.actions.saveFuelSavings = this.props.actions.saveFuelSavings.bind(this);
-    this.onTimeframeChange = this.onTimeframeChange.bind(this);
-    this.save = this.save.bind(this);
-  }
-
-  fuelSavingsKeypress(name, value) {
-    this.props.actions.calculateFuelSavings(this.props, name, value);
-  }
-
-  onTimeframeChange(e) {
-    this.props.actions.calculateFuelSavings(this.props, 'milesDrivenTimeframe', e.target.value);
-  }
-
-  save() {
-    this.props.actions.saveFuelSavings(this.props.fuelSavingsAppState);
-  }
-
-  render() {
-    let settings = this.props.fuelSavingsAppState;
-
-    return (
-      <div>
-        <h2>Fuel Savings Analysis</h2>
-        <table>
-          <tbody>
-          <tr>
-            <td><label htmlFor="newMpg">New Vehicle MPG</label></td>
-            <td><FuelSavingsTextInput onChange={this.fuelSavingsKeypress} name="newMpg" value={settings.newMpg} /></td>
-          </tr>
-          <tr>
-            <td><label htmlFor="tradeMpg">Trade-in MPG</label></td>
-            <td><FuelSavingsTextInput onChange={this.fuelSavingsKeypress} name="tradeMpg" value={settings.tradeMpg} /></td>
-          </tr>
-          <tr>
-            <td><label htmlFor="newPpg">New Vehicle price per gallon</label></td>
-            <td><FuelSavingsTextInput onChange={this.fuelSavingsKeypress} name="newPpg" value={settings.newPpg} /></td>
-          </tr>
-          <tr>
-            <td><label htmlFor="tradePpg">Trade-in price per gallon</label></td>
-            <td><FuelSavingsTextInput onChange={this.fuelSavingsKeypress} name="tradePpg" value={settings.tradePpg} /></td>
-          </tr>
-          <tr>
-            <td><label htmlFor="milesDriven">Miles Driven</label></td>
-            <td>
-              <FuelSavingsTextInput onChange={this.fuelSavingsKeypress} name="milesDriven" value={settings.milesDriven} /> miles per
-              <select name="milesDrivenTimeframe" onChange={this.onTimeframeChange} value={settings.milesDrivenTimeframe}>
-                <option value="week">Week</option>
-                <option value="month">Month</option>
-                <option value="year">Year</option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label>Date Modified</label></td>
-            <td>{settings.dateModified}</td>
-          </tr>
-          </tbody>
-        </table>
-
-        <hr/>
-
-        {settings.necessaryDataIsProvidedToCalculateSavings ? <FuelSavingsResults savings={settings.savings} /> : null}
-        <input type="submit" value="Save" onClick={this.save} />
-      </div>
-    );
-  }
+function buildFuelSavingsKeypress(props) {
+  return function fuelSavingsKeypress(name, value) {
+    props.actions.calculateFuelSavings(props, name, value);
+  };
 }
+
+function buildOnTimeframeChange(props) {
+  return function onTimeframeChange(e) {
+    props.actions.calculateFuelSavings(props, 'milesDrivenTimeframe', e.target.value);
+  };
+}
+
+function buildSave(props) {
+  return function save() {
+    props.actions.saveFuelSavings(props.fuelSavingsAppState);
+  };
+}
+
+const FuelSavingsCalculatorForm = (props) => {
+  const fuelSavingsKeypress = buildFuelSavingsKeypress(props);
+  const onTimeframeChange = buildOnTimeframeChange(props);
+  const save = buildSave(props);
+  const settings = props.fuelSavingsAppState;
+
+  return (
+    <div>
+      <h2>Fuel Savings Analysis</h2>
+      <table>
+        <tbody>
+        <tr>
+          <td><label htmlFor="newMpg">New Vehicle MPG</label></td>
+          <td><FuelSavingsTextInput onChange={fuelSavingsKeypress} name="newMpg" value={settings.newMpg} /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="tradeMpg">Trade-in MPG</label></td>
+          <td><FuelSavingsTextInput onChange={fuelSavingsKeypress} name="tradeMpg" value={settings.tradeMpg} /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="newPpg">New Vehicle price per gallon</label></td>
+          <td><FuelSavingsTextInput onChange={fuelSavingsKeypress} name="newPpg" value={settings.newPpg} /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="tradePpg">Trade-in price per gallon</label></td>
+          <td><FuelSavingsTextInput onChange={fuelSavingsKeypress} name="tradePpg" value={settings.tradePpg} /></td>
+        </tr>
+        <tr>
+          <td><label htmlFor="milesDriven">Miles Driven</label></td>
+          <td>
+            <FuelSavingsTextInput onChange={fuelSavingsKeypress} name="milesDriven" value={settings.milesDriven} /> miles per
+            <select name="milesDrivenTimeframe" onChange={onTimeframeChange} value={settings.milesDrivenTimeframe}>
+              <option value="week">Week</option>
+              <option value="month">Month</option>
+              <option value="year">Year</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td><label>Date Modified</label></td>
+          <td>{settings.dateModified}</td>
+        </tr>
+        </tbody>
+      </table>
+
+      <hr/>
+
+      {settings.necessaryDataIsProvidedToCalculateSavings ? <FuelSavingsResults savings={settings.savings} /> : null}
+      <input type="submit" value="Save" onClick={save} />
+    </div>
+  );
+};
 
 FuelSavingsCalculatorForm.propTypes = {
   actions: PropTypes.object.isRequired,
--- src/components/FuelSavingsTextInput.js
+++ src/components/FuelSavingsTextInput.js
@@ -1,27 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 
-class FuelSavingsTextInput extends Component {
-	constructor(props, context) {
-		super(props, context);
-
-		//why: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
-		this.handleChange = this.handleChange.bind(this);
-	}
-
-	handleChange(e) {
-		this.props.onChange(this.props.name, e.target.value);
-	}
-
-	render() {
-		return (
-			<input className="small"
-				type="text"
-				placeholder={this.props.placeholder}
-				value={this.props.value}
-				onChange={this.handleChange} />
-		);
-	}
+function buildHandleChange(props) {
+  return function handleChange(e) {
+    props.onChange(props.name, e.target.value);
+  };
 }
+
+const FuelSavingsTextInput = (props) => {
+  const handleChange = buildHandleChange(props);
+
+  return (
+    <input className="small"
+      type="text"
+      placeholder={props.placeholder}
+      value={props.value}
+      onChange={handleChange} />
+	);
+};
 
 FuelSavingsTextInput.propTypes = {
 	name: PropTypes.string.isRequired,
