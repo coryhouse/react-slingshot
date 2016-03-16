538a14d67b257c3567ed6c77046bd20e88558854
Leonel Perea <iticleonel.len@gmail.com>
2016-03-15 20:52:22 -0600

    add loader to hanle images url into scss

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,8 @@ const getEntry = function (env) {
 };
 
 const getLoaders = function (env) {
-  const loaders = [{ test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel', 'eslint'] }];
+  const loaders = [{ test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel', 'eslint'] },
+                   { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['file']}];
 
   if (env === productionEnvironment ) {
     // generate separate physical stylesheet for production build using ExtractTextPlugin. This provides separate caching and avoids a flash of unstyled content on load.
