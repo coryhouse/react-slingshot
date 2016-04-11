8aa357e85762b75bf1744711d4f4ad18fb31d49e
Cory House <housecor@gmail.com>
2016-04-11 15:29:03 -0500

    Casing fix.

--- src/reducers/fuelSavings.js
+++ src/reducers/fuelSavings.js
@@ -1,4 +1,4 @@
-import {SAVE_FUEL_SAVINGS, CALCULATE_FUEL_SAVINGS} from '../constants/ActionTypes';
+import {SAVE_FUEL_SAVINGS, CALCULATE_FUEL_SAVINGS} from '../constants/actionTypes';
 import calculator from '../businessLogic/fuelSavingsCalculator';
 import dateHelper from '../businessLogic/dateHelper';
 import objectAssign from 'object-assign';
