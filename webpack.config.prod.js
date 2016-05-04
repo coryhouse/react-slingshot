bad6b9b822952b7f938b1296315c98097235b2ab
Leonel Perea <iticleonel.len@gmail.com>
2016-05-03 22:31:32 -0500

    modify loaders in dev

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,7 +15,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: 'http://localhost:3000/',
+    publicPath: '/',
     filename: 'bundle.js'
   },
   plugins: [
@@ -28,7 +28,10 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /\.(ttf|eot|svg|woff)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file']},
+      {test: /.eot(\?v=\d+.\d+.\d+)?$/, loader: "file"},
+      {test: /.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+      {test: /.ttf(\?v=\d+.\d+.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /.svg(\?v=\d+.\d+.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
       {test: /\.(jpe?g|png|gif)$/i, loaders: ['file']},
       {
         test: /(\.css|\.scss)$/,
