af0ea619fe96bbfb5d480effae3d05b3696ff045
Cory House <housecor@gmail.com>
2016-05-09 08:59:50 -0500

    Removed irrelevant comment

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ export default {
   entry: './src/index',
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
-    path: `${__dirname}/dist`, // Note: Physical files are only output by the production build task `npm run build`.
+    path: `${__dirname}/dist`,
     publicPath: '/',
     filename: 'bundle.js'
   },
