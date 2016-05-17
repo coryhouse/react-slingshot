98700408802f87652dd11cdb82007d12557b9b85
Roland Schilter <roli@schilter.me>
2016-05-17 19:12:18 +0200

    Load styles last for same beahviour as in dev mode

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -22,7 +22,7 @@ fs.readFile('src/index.html', 'utf8', (readError, markup) => {
   const $ = cheerio.load(markup);
 
   // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
-  $('head').prepend('<link rel="stylesheet" href="/styles.css">');
+  $('head').append('<link rel="stylesheet" href="/styles.css">');
 
   if (useTrackJs) {
     if (trackJsToken) {
