a7c95c5cdccf0ec21ce2da6d71e4fb756059dc5b
Cory House <housecor@gmail.com>
2016-07-08 07:45:44 -0500

    Add message when opening prod build

--- tools/distServer.js
+++ tools/distServer.js
@@ -3,6 +3,11 @@
 
 import browserSync from 'browser-sync';
 import historyApiFallback from 'connect-history-api-fallback';
+import {chalkProcessing} from './chalkConfig';
+
+/* eslint-disable no-console */
+
+console.log(chalkProcessing('Opening production build...'));
 
 // Run Browsersync
 browserSync({
