7095e385671b61d93b162973306a03ebd532a367
Paul Jaworski <pjaworski27@gmail.com>
2016-07-28 13:36:00 -0700

    fix root import

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
-import Root from './container/Root';
+import Root from './containers/Root';
 
 const store = configureStore();
 
