7208ab0d52c62fd4e6ec8a6cd41d2761075fd0a6
endlesscombo <endlesscombo@users.noreply.github.com>
2016-08-06 17:23:02 -0700

    Fixes bundle.js not loading on nested page refresh
    1.  Given a nested route: `<Route path="course/:id" component={ManageCoursePage} />`
    2.  If the user tries to go directly to `http://localhost:3000/course/4` in their browser.
    3.  bundle.js will attempt to load from `http://localhost:3000/course/bundle.js` and get a 404 error.
    Found the fix here:  https://github.com/ampedandwired/html-webpack-plugin/issues/98

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -15,6 +15,7 @@ export default {
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: `${__dirname}/src`, // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
     filename: 'bundle.js'
   },
   plugins: [
