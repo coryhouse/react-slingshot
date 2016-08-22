0459059b0c2cc97668faa91048a8c6c9b002ea08
Sheldon Jones <sheldon@lobo.net>
2016-08-22 10:38:01 -0600

    activate redux-immutable-state-invariant during development

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -3,12 +3,16 @@
 // With Redux, the actual stores are in /reducers.
 
 import {createStore, compose, applyMiddleware} from 'redux';
+import reduxImmutableStaeInvariant from 'redux-immutable-state-invariant'
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const middewares = [
     // Add other middleware on this line...
+
+    // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
+    reduxImmutableStaeInvariant(),
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
