4e0b65e17a760ce00e16511c9d166cfbf0a04777
Cory House <housecor@gmail.com>
2016-08-31 21:51:28 -0500

    Switch devtool to resolve (hopefully temporary) bug with cheap-module-eval-source-map

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ export default {
     extensions: ['', '.js', '.jsx']
   },
   debug: true,
-  devtool: 'cheap-module-eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
+  devtool: 'eval-source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
   entry: [
     // must be first entry to properly set public path
