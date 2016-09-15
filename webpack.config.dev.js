4cc8981bf0a4f795bd76619c024f06d28646617f
dtimberlake <daniel@danieltimberlake.com>
2016-09-15 11:05:31 -0500

    Import webpack.config.dev.js
    Need to import `path` to this file before using in the config.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import autoprefixer from 'autoprefixer';
+import path from 'path';
 
 export default {
   resolve: {
