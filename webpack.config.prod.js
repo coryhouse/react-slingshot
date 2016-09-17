90a61b1ff706cc7a0d88bceb773cf524f667b131
Cory House <housecor@gmail.com>
2016-09-16 21:06:18 -0500

    Path fix

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -19,7 +19,7 @@ export default {
   debug: true,
   devtool: 'source-map', // more info:https://webpack.github.io/docs/build-performance.html#sourcemaps and https://webpack.github.io/docs/configuration.html#devtool
   noInfo: true, // set to false to see a list of every file being bundled.
-  entry: path.resolve(__dirname, '/src/index'),
+  entry: path.resolve(__dirname, 'src/index'),
   target: 'web', // necessary per https://webpack.github.io/docs/testing.html#compile-and-test
   output: {
     path: path.resolve(__dirname, 'dist'),
