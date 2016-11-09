4201b52166149eba80a446731517e7ef4532fdd7
Nick Taylor <nick@weareadaptive.com>
2016-11-09 09:41:52 -0500

    Upgrade for the Redux DevTools extension.

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -19,9 +19,9 @@ export default function configureStore(initialState) {
     thunkMiddleware,
   ];
 
-  const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares),
-    window.devToolsExtension ? window.devToolsExtension() : f => f // add support for Redux dev tools
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(...middlewares)
     )
   );
 
