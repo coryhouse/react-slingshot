4b23e2cae305ea1052803fbea073e044a6ed9f1f
Cory House <housecor@gmail.com>
2016-12-03 18:11:55 -0600

    Remove cruft

--- src/webpack-public-path.js
+++
@@ -1,9 +0,0 @@
-// Dynamically set the webpack public path at runtime below
-// This magic global is used by webpack to set the public path at runtime.
-// The public path is set dynamically to avoid the following issues:
-// 1. https://github.com/coryhouse/react-slingshot/issues/205
-// 2. https://github.com/coryhouse/react-slingshot/issues/181
-// 3. https://github.com/coryhouse/react-slingshot/pull/125
-// Documentation: http://webpack.github.io/docs/configuration.html#output-publicpath
-// eslint-disable-next-line no-undef
-__webpack_public_path__ = window.location.protocol + "//" + window.location.host + "/";
--- tools/build.js
+++ tools/build.js
@@ -1,35 +1,10 @@
-// More info on Webpack's Node API here: https://webpack.github.io/docs/node.js-api.html
-// Allowing console calls below since this is a build file.
 /* eslint-disable no-console */
 import webpack from 'webpack';
 import config from '../webpack.config.prod';
-import {chalkError, chalkSuccess, chalkWarning, chalkProcessing} from './chalkConfig';
 
-process.env.NODE_ENV = 'production'; // this assures React is built in prod mode and that the Babel dev config doesn't apply.
-
-console.log(chalkProcessing('Generating minified bundle. This will take a moment...'));
+console.log('Generating bundle...');
 
 webpack(config).run((error, stats) => {
-  if (error) { // so a fatal error occurred. Stop here.
-    console.log(chalkError(error));
-    return 1;
-  }
-
-  const jsonStats = stats.toJson();
-
-  if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(chalkError(error)));
-  }
-
-  if (jsonStats.hasWarnings) {
-    console.log(chalkWarning('Webpack generated the following warnings: '));
-    jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
-  }
-
-  console.log(`Webpack stats: ${stats}`);
-
-  // if we got this far, the build succeeded.
-  console.log(chalkSuccess('Your app is compiled in production mode in /dist. It\'s ready to roll!'));
-
+  console.log(chalkSuccess('Your app is compiled!'));
   return 0;
 });
