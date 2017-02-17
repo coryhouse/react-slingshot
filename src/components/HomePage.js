4fb5d3ed94a77d2982fad78085988dbddc18bb17
arshdkhn1 <arshdkhn1@gmail.com>
2017-02-18 01:06:30 +0530

    Adding sass indented syntax support.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import '../styles/homepage.sass';
 
 const HomePage = () => {
   return (
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -46,7 +46,7 @@ export default {
         context: '/',
         postcss: () => [autoprefixer],
       }
-    }) 
+    })
   ],
   module: {
     rules: [
@@ -57,7 +57,7 @@ export default {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
       {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'},
       {test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'},
-      {test: /(\.css|\.scss)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
+      {test: /(\.css|\.scss|\.sass)$/, loaders: ['style-loader', 'css-loader?sourceMap', 'postcss-loader', 'sass-loader?sourceMap']}
     ]
   }
 };
