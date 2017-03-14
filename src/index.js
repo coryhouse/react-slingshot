b1ab8ae5f16507805bc4b222679f8bc9eccd813b
oshalygin <oshalygin@gmail.com>
2017-03-14 16:40:20 -0700

    Add react hot loader 3
    - This commit wires up react-hot-loader 3 to
      be used in the application.  There are numerous
      benefits to the latest release, all of which
      can be seen at https://github.com/gaearon/react-hot-loader
    - Note that the specific implementation around
      wrapping in a Root component is part of how
      react-hot-loader 3 needs to be configured.
    - Note that the package is brought in as a
      dependency, not a dev dependency because of
      how it is switched at runtime or not.
    - More information on the migration can be
      viewed at:
    https://github.com/gaearon/react-hot-loader/tree/master/docs\#migration-to-30
    
    Related: #216

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/default */
 
 import React from 'react';
-import {render} from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
+import Root from './components/Root';
+
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
@@ -15,8 +16,22 @@ const store = configureStore();
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
+
 render(
-  <Provider store={store}>
-    <Router history={history} routes={routes} />
-  </Provider>, document.getElementById('app')
+  <AppContainer>
+    <Root store={store} history={history} />
+  </AppContainer>,
+  document.getElementById('app')
 );
+
+if (module.hot) {
+  module.hot.accept('./components/Root', () => {
+    const NewRoot = require('./components/Root').default;
+    render(
+      <AppContainer>
+        <NewRoot store={store} history={history} />
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
