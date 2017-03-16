180e6698823b367813c095641af81b80ad44683e
TobiahRex <tobiah.rex@gmail.com>
2017-03-16 09:42:29 +0900

    removed react-router form package.json & installed react-router-dom.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,38 @@
 import React from 'react';
 import {render} from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+// import { Router, browserHistory } from 'react-router'; NOTE - rrV4 -
+import { Route } from 'react-router';  // NOTE - rrV4 -
+import createHistory from 'history/createBrowserHistory'; // NOTE - rrV4 -
+// import routes from './routes'; NOTE - rrv4 -
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
+
+import App from './components/App';
+import HomePage from './components/HomePage';
+import FuelSavingsPage from './containers/FuelSavingsPage'; // eslint-disable-line import/no-named-as-default
+import AboutPage from './components/AboutPage';
+import NotFoundPage from './components/NotFoundPage';
+
+// import { syncHistoryWithStore } from 'react-router-redux'; NOTE - rrV4 -
+import { ConnectedRouter } from 'react-router-redux'; // NOTE - rrV4 -
 
 const store = configureStore();
-
-// Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+const history = createHistory(); // NOTE - rrV4 -
 
 render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    {/* <Router history={history} routes={routes} /> NOTE - rrv4 - */}
+    <ConnectedRouter history={history}>
+      <div>
+        <Route path="/" component={App}>
+          {/* <IndexRoute component={HomePage}/> */}
+          <Route path="fuel-savings" component={FuelSavingsPage}/>
+          <Route path="about" component={AboutPage}/>
+          <Route path="*" component={NotFoundPage}/>
+        </Route>
+      </div>
+    </ConnectedRouter>
   </Provider>, document.getElementById('app')
 );
