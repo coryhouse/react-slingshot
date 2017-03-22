d3e51e2132f92f3ea9d6d6999b3304efb4a20886
TobiahRex <tobiah.rex@gmail.com>
2017-03-22 15:05:10 +0900

    modified middleware array @ store/configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -7,16 +7,15 @@ import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 export const history = createHistory();
 function configureStoreProd(initialState) {
+  const reactRouterMiddleware = routerMiddleware(history);
   const middlewares = [
     // Add other middleware on this line...
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
+    reactRouterMiddleware,
   ];
-
-  const reactRouterMiddleware = routerMiddleware(history);
-  middlewares.push(reactRouterMiddleware);
 
   return createStore(rootReducer, initialState, compose(
     applyMiddleware(...middlewares)
@@ -25,6 +24,7 @@ function configureStoreProd(initialState) {
 }
 
 function configureStoreDev(initialState) {
+  const reactRouterMiddleware = routerMiddleware(history);
   const middlewares = [
     // Add other middleware on this line...
 
@@ -34,10 +34,8 @@ function configureStoreDev(initialState) {
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
+    reactRouterMiddleware,
   ];
-
-  const reactRouterMiddleware = routerMiddleware(history);
-  middlewares.push(reactRouterMiddleware);
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
   const store = createStore(rootReducer, initialState, composeEnhancers(
