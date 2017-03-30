3c21567bb516d9d603f4bd6fccc12e0caa0ad1fa
Oleg Shalygin <oshalygin@gmail.com>
2017-03-29 20:49:18 -0700

    Add react hot loader 3 (#392)
    - This commit wires up react-hot-loader 3 to
      be used in the application.  There are numerous
      benefits to the latest release, all of which
      can be seen at https://github.com/gaearon/react-hot-loader
    - Note that the specific implementation around
      wrapping in a Root component is part of how
      react-hot-loader 3 needs to be configured.
    - Note that the package is brought in as a
      dependency, not a dev dependency because of
      how it is switched at runtime or not.
    - More information on the migration can be
      viewed at:
    https://github.com/gaearon/react-hot-loader/tree/master/docs\#migration-to-30
    
    Related: #216

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,6 +11,7 @@ export default {
   entry: [
     // must be first entry to properly set public path
     './src/webpack-public-path',
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client?reload=true',
     path.resolve(__dirname, 'src/index.js') // Defining path seems necessary for this to work consistently on Windows machines.
   ],
