a2b6bca14219e52dfa28d4b61afdf3d8585342a4
TobiahRex <tobiah.rex@gmail.com>
2017-04-02 09:14:55 +0900

    updated master branch.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-
 import Root from './components/Root';
 import configureStore, { history } from './store/configureStore';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
