e1c3f18b0c67e9e205528021150f92b5f16977c6
Mike DeVita <mike@relative.media>
2017-04-30 19:41:10 -0700

    Actually copy the favicon to dist/ on prod build (#424)
    * include favicon in HtmlWebpackPlugin()
    
    this helps to resolve #128, prod build wasn't copying the favicon.ico to dist/
    
    * Update index.ejs
    
    * Revert "Update index.ejs"
    
    This reverts commit fdc4a2fc81f6c61ae5289ec873f41d2708ce3f71.

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,6 +37,7 @@ export default {
     // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
     new HtmlWebpackPlugin({
       template: 'src/index.ejs',
+      favicon: 'src/favicon.ico',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
