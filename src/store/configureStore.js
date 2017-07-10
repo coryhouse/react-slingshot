520320a5493b5a7b3ea0c1e409b363dbd168ebae
Arun Sood <arsood@gmail.com>
2017-07-10 14:37:46 -0400

    Add redux-logger to dev store configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,7 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from '../reducers';
 
 function configureStoreProd(initialState) {
@@ -28,6 +29,9 @@ function configureStoreDev(initialState) {
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
+
+    // Middleware to print Redux store updates to the console
+    logger,
   ];
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
