d11675633e97c79ed06e1e63e6b663255b959c0b
TobiahRex <tobiah.rex@gmail.com>
2017-08-31 14:44:39 +0900

    fixed conflicts with upstream master.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, IndexLink } from 'react-router';
-
+console.log('IndexLink: ', IndexLink)
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
 // component at the top-level.
--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
+import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
-
-import configureStore from './store/configureStore';
-require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
-
+require('./favicon.ico'); // Tell webpack to load favicon.ico
 const store = configureStore();
-
-// Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
-
 
 render(
   <AppContainer>
