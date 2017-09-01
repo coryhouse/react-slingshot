975fe9e28d51a2896cb9ab1afc9290b43b792fb4
TobiahRex <tobiah.rex@gmail.com>
2017-09-01 14:14:24 +0900

    comment spell check & de-console on Root.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,7 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
-// 'routerMiddleware': the new way of storing route changes with redux middlware since rrV4.
+// 'routerMiddleware': the new way of storing route changes with redux middleware since rrV4.
 import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 export const history = createHistory();
