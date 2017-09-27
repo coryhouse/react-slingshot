083b6242451eca6fc6f58cc39f839680c943e818
Cory House <Cory.House@coxautoinc.com>
2017-09-27 18:01:43 -0500

    refactor(debugging): Renable faster sourcemaps since Chrome supports again

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ export default {
   resolve: {
     extensions: ['*', '.js', '.jsx', '.json']
   },
-  devtool: 'eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
+  devtool: 'cheap-module-eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
   entry: [
     // must be first entry to properly set public path
     './src/webpack-public-path',
