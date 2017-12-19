e6845e0cf1d77aefb35f7eeafb04d73d1b2f5432
Cory House <Cory.House@coxautoinc.com>
2017-12-18 21:07:48 -0600

    Remove experimental test - accidentally committed

--- src/components/containers/FuelSavingsPage.spec.js
+++ src/components/containers/FuelSavingsPage.spec.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
-import * as actions from "../../actions/fuelSavingsActions";
-import ConnectedFuelSavingsPage, { FuelSavingsPage } from "./FuelSavingsPage";
+import {FuelSavingsPage} from "./FuelSavingsPage";
 import FuelSavingsForm from "../FuelSavingsForm";
-import { fuelSavings } from "../../types/index";
 
 describe("<FuelSavingsPage />", () => {
   it("should contain <FuelSavingsForm />", () => {
@@ -17,13 +15,5 @@ describe("<FuelSavingsPage />", () => {
     );
 
     expect(wrapper.find(FuelSavingsForm).length).toEqual(1);
-  });
-
-  // App integration test
-  it("should display results when populated", () => {
-    const wrapper = shallow(
-      <ConnectedFuelSavingsPage actions={actions} fuelSavings={fuelSavings} />
-    ).dive(); // Use dive to get reference to child component.
-    expect(wrapper.find(".results")).toBe(true);
   });
 });
