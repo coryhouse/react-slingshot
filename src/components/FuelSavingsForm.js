d823c56fd951bc1cf90c60cdc2171e9973425a13
Cory House <Cory.House@coxautoinc.com>
2017-12-31 17:11:09 -0600

    Add IDs to support short selectors in integration tests

--- src/components/FuelSavingsForm.js
+++ src/components/FuelSavingsForm.js
@@ -11,33 +11,35 @@ const FuelSavingsForm = ({fuelSavings, onSaveClick, onChange}) => (
       <tbody>
         <tr>
           <td><label htmlFor="newMpg">New Vehicle MPG</label></td>
-          <td><FuelSavingsTextInput onChange={onChange} name="newMpg" value={fuelSavings.newMpg}/>
+          <td><FuelSavingsTextInput id="newMpg" onChange={onChange} name="newMpg" value={fuelSavings.newMpg}/>
           </td>
         </tr>
         <tr>
           <td><label htmlFor="tradeMpg">Trade-in MPG</label></td>
-          <td><FuelSavingsTextInput onChange={onChange} name="tradeMpg" value={fuelSavings.tradeMpg}/>
+          <td><FuelSavingsTextInput id="tradeMpg" onChange={onChange} name="tradeMpg" value={fuelSavings.tradeMpg}/>
           </td>
         </tr>
         <tr>
           <td><label htmlFor="newPpg">New Vehicle price per gallon</label></td>
-          <td><FuelSavingsTextInput onChange={onChange} name="newPpg" value={fuelSavings.newPpg}/>
+          <td><FuelSavingsTextInput id="newPpg" onChange={onChange} name="newPpg" value={fuelSavings.newPpg}/>
           </td>
         </tr>
         <tr>
           <td><label htmlFor="tradePpg">Trade-in price per gallon</label></td>
-          <td><FuelSavingsTextInput onChange={onChange} name="tradePpg" value={fuelSavings.tradePpg}/>
+          <td><FuelSavingsTextInput id="tradePpg" onChange={onChange} name="tradePpg" value={fuelSavings.tradePpg}/>
           </td>
         </tr>
         <tr>
           <td><label htmlFor="milesDriven">Miles Driven</label></td>
           <td>
             <FuelSavingsTextInput
+              id="milesDriven"
               onChange={onChange}
               name="milesDriven"
               value={fuelSavings.milesDriven}/>
             miles per
             <select
+              id="milesDrivenTimeframe"
               name="milesDrivenTimeframe"
               onChange={onChange}
               value={fuelSavings.milesDrivenTimeframe}>
