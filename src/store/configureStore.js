44d5165f270032238ecdc35fcc0fbc8841dc9f56
Brian Reed <me@iambrian.com>
2018-05-10 14:50:27 -0400

    Updated documentation links (#565)
    * Updated thunk documentation link
    
    * Update configureStore.js

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -12,7 +12,7 @@ function configureStoreProd(initialState) {
     // Add other middleware on this line...
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+    // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
     thunk,
     reactRouterMiddleware,
   ];
@@ -32,7 +32,7 @@ function configureStoreDev(initialState) {
     reduxImmutableStateInvariant(),
 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
-    // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
+    // https://github.com/reduxjs/redux-thunk#injecting-a-custom-argument
     thunk,
     reactRouterMiddleware,
   ];
