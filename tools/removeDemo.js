82aa485e9db426bd91e64ccb0e9db0b98fd20246
Kalyan Kankala <kankala.kalyan@gmail.com>
2020-03-29 20:22:02 -0400

    removing snapshots folders inside src

--- tools/removeDemo.js
+++ tools/removeDemo.js
@@ -1,37 +1,40 @@
 // This script removes demo app files
-import rimraf from 'rimraf';
-import fs from 'fs';
-import {chalkSuccess} from './chalkConfig';
+import rimraf from "rimraf";
+import fs from "fs";
+import { chalkSuccess } from "./chalkConfig";
 
 /* eslint-disable no-console */
 
 const pathsToRemove = [
-  './src/actions/*',
-  './src/utils',
-  './src/components/*',
-  './src/constants/*',
-  './src/containers/*',
-  './src/images',
-  './src/reducers/*',
-  './src/store/store.spec.js',
-  './src/styles/*',
-  './src/index.js',
-  './src/types/*',
-  './tools/removeDemo.js'
+  "./src/actions/*",
+  "./src/utils",
+  "./src/components/*",
+  "./src/constants/*",
+  "./src/containers/*",
+  "./src/images",
+  "./src/reducers/*",
+  "./src/store/store.spec.js",
+  "./src/styles/*",
+  "./src/index.js",
+  "./src/types/*",
+  "./tools/removeDemo.js",
+  "./src/**/__snapshots__"
 ];
 
 const filesToCreate = [
   {
-    path: './src/components/emptyTest.spec.js',
-    content: '// Must have at least one test file in this directory or Mocha will throw an error.'
+    path: "./src/components/emptyTest.spec.js",
+    content:
+      "// Must have at least one test file in this directory or Mocha will throw an error."
   },
   {
-    path: './src/index.js',
-    content: '// Set up your application entry point here...'
+    path: "./src/index.js",
+    content: "// Set up your application entry point here..."
   },
   {
-    path: './src/reducers/index.js',
-    content: '// Set up your root reducer here...\n import { combineReducers } from \'redux\';\n export default combineReducers;'
+    path: "./src/reducers/index.js",
+    content:
+      "// Set up your root reducer here...\n import { combineReducers } from 'redux';\n export default combineReducers;"
   }
 ];
 
@@ -49,25 +52,25 @@ function createFile(file) {
 }
 
 function removePackageJsonScriptEntry(scriptName) {
-  const packageJsonPath = './package.json';
+  const packageJsonPath = "./package.json";
   let fileData = fs.readFileSync(packageJsonPath);
   let content = JSON.parse(fileData);
   delete content.scripts[scriptName];
-  fs.writeFileSync(packageJsonPath,
-    JSON.stringify(content, null, 2) + '\n');
+  fs.writeFileSync(packageJsonPath, JSON.stringify(content, null, 2) + "\n");
 }
 
 let numPathsRemoved = 0;
 pathsToRemove.map(path => {
   removePath(path, () => {
     numPathsRemoved++;
-    if (numPathsRemoved === pathsToRemove.length) { // All paths have been processed
+    if (numPathsRemoved === pathsToRemove.length) {
+      // All paths have been processed
       // Now we can create files since we're done deleting.
       filesToCreate.map(file => createFile(file));
     }
   });
 });
 
-removePackageJsonScriptEntry('remove-demo');
+removePackageJsonScriptEntry("remove-demo");
 
-console.log(chalkSuccess('Demo app removed.'));
+console.log(chalkSuccess("Demo app removed."));
